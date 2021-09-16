f3097e9ecc88cc56a5a90dae612ee44d07df9446
Viktor Kodet <viktor.kodet@gmail.com>
2021-09-16 13:58:33 +0200

    Initial commit

---
+++ src/test/java/com/example/kuberneteslabb/KuberneteslabbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kuberneteslabb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberneteslabbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
